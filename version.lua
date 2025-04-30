9c2f4f560b05b2c7a1c1bf733648e3fbc453962d
Eming Kwok <EMKCentric@gmail.com>
2025-04-30 19:04:44 +0800

    0.1.0

--- version.lua
+++ version.lua
@@ -1,8 +1,1 @@
---[[
-  Copyright (c) 2025 Eming Kwok
-
-  This source code is licensed under the MIT license found in the
-  LICENSE file in the root directory of this source tree.
-]]
-
-return '0.2.0-alpha.2'
+return '0.1.0'
